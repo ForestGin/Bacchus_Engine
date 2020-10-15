a567a330722bedc8f96ae1aa3fa5c547d6f33248
Pol Bosch <pol.hcsob@gmail.com>
2020-10-15 22:37:17 +0200

    Revert "Added some geomtry shapes"
    This reverts commit 67f6088240807ffde5cf346aeb51941cc5884fdc.

--- Bacchus_Engine/ModuleSceneIntro.h
+++ Bacchus_Engine/ModuleSceneIntro.h
@@ -21,24 +21,6 @@ public:
 public:
 	uint cube_id;//array 
 
-	//Cube
-	uint cube_verticesID;
-	uint cube_buffIndicesID;
-
-	//Tetrahedron
-	uint tetrahedron_verticesID;
-	uint tetrahedron_buffIndicesID;
-
-	//Octahedron
-	uint octahedron_verticesID;
-	uint octahedron_buffIndicesID;
-
-	//Icosahedron
-	uint icosahedron_verticesID;
-	uint icosahedron_buffIndicesID;
-
-	//Dodecahedron
-	uint dodecahedron_verticesID;
-	uint dodecahedron_buffIndicesID;
-
+	uint indices_id;
+	uint buffIndicesID;
 };
