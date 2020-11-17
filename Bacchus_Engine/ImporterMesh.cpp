e8f5c760af3015ef2d083e61c93fbc6ef5fd14cd
Pol Bosch <pol.hcsob@gmail.com>
2020-11-17 23:30:33 +0100

    Updated Import function to match primitives method

--- Bacchus_Engine/ImporterMesh.cpp
+++ Bacchus_Engine/ImporterMesh.cpp
@@ -25,7 +25,7 @@ bool ImporterMesh::Import(const ImportData& IData) const
 
 	ImportMeshData data = (ImportMeshData&)IData;
 
-	
+	//Vertices
 	data.new_mesh->VerticesSize = data.mesh->mNumVertices;
 	data.new_mesh->Vertices = new float3[data.mesh->mNumVertices];
 
@@ -36,12 +36,9 @@ bool ImporterMesh::Import(const ImportData& IData) const
 		data.new_mesh->Vertices[i].z = data.mesh->mVertices[i].z;
 	}
 
-	glGenBuffers(1, (GLuint*)&data.new_mesh->VerticesID); // create buffer
-	glBindBuffer(GL_ARRAY_BUFFER, data.new_mesh->VerticesID); // start using created buffer
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float3) * data.new_mesh->VerticesSize, data.new_mesh->Vertices, GL_STATIC_DRAW); // send vertices to VRAM
-	glBindBuffer(GL_ARRAY_BUFFER, 0); // Stop using buffer
+	data.new_mesh->VerticesID = App->renderer3D->CreateBufferFromData(GL_ARRAY_BUFFER, sizeof(float3) * data.new_mesh->VerticesSize, data.new_mesh->Vertices);
 	
-
+	//Normals
 	if (data.mesh->HasNormals())
 	{
 		data.new_mesh->NormalsSize = data.mesh->mNumVertices;
@@ -56,6 +53,7 @@ bool ImporterMesh::Import(const ImportData& IData) const
 
 	}
 
+	//TexCoords
 	if (data.mesh->HasTextureCoords(0))
 	{
 		data.new_mesh->TexCoords = new float[data.mesh->mNumVertices * 2];
@@ -68,12 +66,9 @@ bool ImporterMesh::Import(const ImportData& IData) const
 		
 	}
 
-	glGenBuffers(1, (GLuint*)&data.new_mesh->TextureCoordsID); // create buffer
-	glBindBuffer(GL_ARRAY_BUFFER, data.new_mesh->TextureCoordsID); // start using created buffer
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * data.new_mesh->VerticesSize * 2, data.new_mesh->TexCoords, GL_STATIC_DRAW); // send vertices to VRAM
-	glBindBuffer(GL_ARRAY_BUFFER, 0); // Stop using buffer
+	data.new_mesh->TextureCoordsID = App->renderer3D->CreateBufferFromData(GL_ARRAY_BUFFER, sizeof(float) * data.new_mesh->VerticesSize * 2, data.new_mesh->TexCoords);
 
-
+	//Indices
 	data.new_mesh->IndicesSize = data.mesh->mNumFaces * 3;
 	data.new_mesh->Indices = new uint[data.new_mesh->IndicesSize];
 
@@ -92,11 +87,8 @@ bool ImporterMesh::Import(const ImportData& IData) const
 		data.new_mesh->Indices[j * 3 + 2] = face.mIndices[2];
 	}
 
-	glGenBuffers(1, (GLuint*)&data.new_mesh->IndicesID); // create buffer
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, data.new_mesh->IndicesID); // start using created buffer
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * data.new_mesh->IndicesSize, data.new_mesh->Indices, GL_STATIC_DRAW); // send vertices to VRAM
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // Stop using buffer
-
+	data.new_mesh->IndicesID = App->renderer3D->CreateBufferFromData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * data.new_mesh->IndicesSize, data.new_mesh->Indices);
+	
 	return true;
 }
 
